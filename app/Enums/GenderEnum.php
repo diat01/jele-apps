ec3c24659d9a232318b6dbf2c252a3a33739f593
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-05-14 11:25:24 +0500

    Add gender attribute to User model
    Introduced a new `gender` field to the `users` table, along with its integration into the User model. A `GenderEnum` enum is used to standardize gender values, and the corresponding database migration was added to handle schema changes.

--- app/Enums/GenderEnum.php
+++ app/Enums/GenderEnum.php
@@ -7,9 +7,4 @@ enum GenderEnum: string
     case MALE = 'male';
     case FEMALE = 'female';
     case OTHER = 'other';
-
-    public static function values(): array
-    {
-        return array_column(self::cases(), 'value');
-    }
 }
